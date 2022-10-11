db86a9e1d563c97390e1653d40a4e019731268fd
Hajime Fujita <hajime.fujita@gmail.com>
2022-10-11 18:21:41 +0900

    いくつか問題を解いた

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,29 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: u32,
-        mut d: [i32; n],
+        n: usize,
+        a: [[i32; n]; n],
     }
-
-    d.sort_by(|a, b| b.partial_cmp(a).unwrap());
-
-    let mut ans = 0;
-    for i in 0..n {
-        if i == 0 {
-            ans += 1;
-        } else {
-            if d[i as usize] < d[(i - 1) as usize] {
-                ans += 1;
-            }
-        }
-    }
-
-    println!("{}", ans);
 }
 
 // let mut v = vec![0; 2];  // 要素数2のベクタを0で初期化
+// let mut v: Vec<i32> = Vec::new();
+
+// let a = String::new();  // 新しい空のStringを生成
+// let a = String::from("This is a "); // 文字列を代入しながら初期化
+// let a = "This is a ".to_string();
 
 // a.sort_by(|a, b| b.partial_cmp(a).unwrap());  // 降順にソート
 
 // input! {
 //     n: u32,
 //     a: [i32; n],
+// }
+
+// input! {
+//     n: usize,
+//     m: usize,
+//     a: [[i32; n]; m],
 // }
 
 // use proconio::input;
@@ -43,46 +39,4 @@ fn main() {
 //     let num3 = c[2] as i32 - 48;
 
 //     println!("{:?}", num1 + num2 + num3);
-// }
-
-// fn input_pattern01() -> (Vec<i32>, Vec<i32>) {
-//     //
-//     // 最初に要素数を入力
-//     // 次に複数の値を入力
-//     // 3
-//     // 1 2
-//     // 3 4
-//     // 5 6
-//     //
-//     let s = {
-//         let mut s = String::new();
-//         std::io::stdin().read_line(&mut s).unwrap();
-//         s.trim_end().to_owned()
-//     };
-
-//     let n = {
-//         let mut ws = s.split_whitespace();
-//         let n: i32 = ws.next().unwrap().parse().unwrap();
-//         n
-//     };
-
-//     let mut m1: Vec<i32> = Vec::new();
-//     let mut m2: Vec<i32> = Vec::new();
-
-//     for _ in 0..n {
-//         let s = {
-//             let mut s = String::new();
-//             std::io::stdin().read_line(&mut s).unwrap();
-//             s.trim_end().to_owned()
-//         };
-
-//         let mut ws = s.split_whitespace();
-//         let n: i32 = ws.next().unwrap().parse().unwrap();
-//         let m: i32 = ws.next().unwrap().parse().unwrap();
-
-//         m1.push(n);
-//         m2.push(m);
-//     }
-
-//     return (m1, m2);
 // }
